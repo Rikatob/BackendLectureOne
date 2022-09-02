75644dc7c302e77e8b005700229260863894e910
Rikatob <89411878+Rikatob@users.noreply.github.com>
2022-09-02 11:03:50 +0200

    🔴 set up for first test, test fir number 1.

---
+++ src/test/java/lectureOneTests/RomanNumberTests.java
@@ -0,0 +1,19 @@
+package lectureOneTests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RomanNumberTests {
+
+    @Test
+    void shouldReturnIfor1(){
+        assertEquals("I", toRomanNumber(1));
+    }
+
+    private String toRomanNumber(int n) {
+
+
+        return null;
+    }
+}
