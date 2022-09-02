a8bf4081043a70a4c26b6a502b8e8fdb54b9c724
Rikatob <89411878+Rikatob@users.noreply.github.com>
2022-09-02 11:35:45 +0200

    🟢 Roman number for 3 OK.

--- src/test/java/lectureOneTests/RomanNumberTests.java
+++ src/test/java/lectureOneTests/RomanNumberTests.java
@@ -7,23 +7,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RomanNumberTests {
 
     @Test
-    void shouldReturnIfor1(){
+    void shouldReturnIfor1() {
         assertEquals("I", toRomanNumber(1));
     }
 
     @Test
-    void shouldReturnIIfor2(){
+    void shouldReturnIIfor2() {
         assertEquals("II", toRomanNumber(2));
     }
 
     @Test
-    void shouldReturnIIIfor3(){
+    void shouldReturnIIIfor3() {
         assertEquals("III", toRomanNumber(3));
     }
+
     private String toRomanNumber(int n) {
         String result = "I";
 
-        if(n == 2){
+        if (n >= 2) {
+            result += "I";
+        }
+        if (n >= 3) {
             result += "I";
         }
 
